77666eb350658690230e8c1c91b3dcbbd221c6ab
Sarthak Sahu <53469278+SarthakSahu1009@users.noreply.github.com>
2022-10-11 00:41:14 +0530

    krde

--- app/src/Blog/home/forms.py
+++ app/src/Blog/home/forms.py
@@ -5,7 +5,7 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('phone_no', 'bio', 'facebook', 'instagram', 'linkedin', 'image', )
-     
+     #hehe
         
 class BlogPostForm(forms.ModelForm):
     class Meta:
